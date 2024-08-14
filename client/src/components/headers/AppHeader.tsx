72182d173cb6df8c4c97a8bb45bf13443cb29e8a
CubeCoding <sljohnston714@gmail.com>
2024-08-13 22:16:04 -0400

    implemented css modules for no style overlapping

--- client/src/components/headers/AppHeader.tsx
+++ client/src/components/headers/AppHeader.tsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/pro-solid-svg-icons";
+
 function AppHeader() {
-  return <header></header>;
+  const handleCollapseClick = () => {
+    // a script to collapse/uncollapse the sidebar
+  };
+
+  return (
+    <header>
+      <div>
+        <button onClick={handleCollapseClick} className="nav-link">
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <Link to="/app" className="logo">
+          TaskSync
+        </Link>
+      </div>
+    </header>
+  );
 }
 
 export default AppHeader;
--- client/src/components/headers/HomeHeader.tsx
+++ client/src/components/headers/HomeHeader.tsx
@@ -1,30 +1,30 @@
 import { Link } from "react-router-dom";
-import "./HomeHeader.css";
+import styles from "./HomeHeader.module.css";
 
 function HomeHeader() {
   return (
-    <header>
-      <div className="logo">
+    <header className={styles.header}>
+      <div className={styles.logo}>
         <img src="/img/logo.svg" alt="" />
       </div>
-      <div className="title">
+      <div className={styles.title}>
         <Link to="/">TaskSync</Link>
       </div>
       <nav>
-        <Link to="/signup" className="sign_up_button nav_button">
+        <Link to="/signup" className={styles.sign_up_button}>
           Sign up for free
         </Link>
-        <Link to="/login" className="nav_button">
+        <Link to="/login" className={styles.nav_button}>
           Log in
         </Link>
-        <div className="header-divider"></div>
-        <Link to="/help" className="nav_button">
+        <div className={styles.headerDivider}></div>
+        <Link to="/help" className={styles.nav_button}>
           Help
         </Link>
-        <Link to="/download" className="nav_button">
+        <Link to="/download" className={styles.nav_button}>
           Download
         </Link>
-        <Link to="/features" className="nav_button">
+        <Link to="/features" className={styles.nav_button}>
           Features
         </Link>
       </nav>
