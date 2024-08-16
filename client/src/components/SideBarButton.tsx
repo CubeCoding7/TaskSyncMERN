4cab275a87e8a49e14ef9a43355383ec9e53718e
CubeCoding <sljohnston714@gmail.com>
2024-08-16 14:14:08 -0400

    made the sidebar button a component

---
+++ client/src/components/SideBarButton.tsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+import styles from "./navigation/SideBar.module.css";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+interface Props {
+  href: string;
+  icon: IconProp;
+}
+
+export default function SideBarButton({ href, icon }: Props) {
+  return (
+    <NavLink
+      to={href}
+      className={({ isActive }) =>
+        isActive ? `${styles.iconLink} ${styles.active}` : styles.iconLink
+      }
+    >
+      <div className={styles.iconActiveLine}></div>
+      <FontAwesomeIcon icon={icon} />
+    </NavLink>
+  );
+}
--- client/src/components/navigation/SideBar.tsx
+++ client/src/components/navigation/SideBar.tsx
@@ -1,54 +1,21 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
   faTasks,
   faCalendar,
   faStickyNote,
 } from "@fortawesome/pro-solid-svg-icons";
-import styles from "./SideBar.module.css"; // Adjust path if necessary
+import styles from "./SideBar.module.css";
+import SideBarButton from "../SideBarButton";
 
 const Sidebar: React.FC = () => {
   return (
     <div className={styles.sidebar}>
       <div className={styles.iconSection}>
-        <NavLink
-          to="/app/home"
-          className={({ isActive }) =>
-            isActive ? `${styles.iconLink} ${styles.active}` : styles.iconLink
-          }
-        >
-          <div className={styles.iconActiveLine}></div>
-          <FontAwesomeIcon icon={faHome} />
-        </NavLink>
-        <NavLink
-          to="/app/tasks"
-          className={({ isActive }) =>
-            isActive ? `${styles.iconLink} ${styles.active}` : styles.iconLink
-          }
-        >
-          <div className={styles.iconActiveLine}></div>
-          <FontAwesomeIcon icon={faTasks} />
-        </NavLink>
-        <NavLink
-          to="/app/calendar"
-          className={({ isActive }) =>
-            isActive ? `${styles.iconLink} ${styles.active}` : styles.iconLink
-          }
-        >
-          <div className={styles.iconActiveLine}></div>
-          <FontAwesomeIcon icon={faCalendar} />
-        </NavLink>
-        <NavLink
-          to="/app/notes"
-          className={({ isActive }) =>
-            isActive ? `${styles.iconLink} ${styles.active}` : styles.iconLink
-          }
-        >
-          <div className={styles.iconActiveLine}></div>
-          <FontAwesomeIcon icon={faStickyNote} />
-        </NavLink>
+        <SideBarButton href="/app/home" icon={faHome} />
+        <SideBarButton href="/app/tasks" icon={faTasks} />
+        <SideBarButton href="/app/calendar" icon={faCalendar} />
+        <SideBarButton href="/app/notes" icon={faStickyNote} />
       </div>
     </div>
   );
