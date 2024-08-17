ba8c4cd95ec7c22abc1cff8ef71d4a9063d8463a
CubeCoding <sljohnston714@gmail.com>
2024-08-17 11:08:24 -0400

    added properties to the widgets

--- client/src/pages/AppPage.tsx
+++ client/src/pages/AppPage.tsx
@@ -1,10 +1,14 @@
-import Widget from "../components/Widget";
+import { faCalendar, faStickyNote } from "@fortawesome/pro-solid-svg-icons";
+import WidgetSmall from "../components/WidgetSmall";
+import WidgetLarge from "../components/WidgetLarge";
+import styles from "../components/Widget.module.css";
 
 function AppPage() {
   return (
-    <>
-      <Widget />
-    </>
+    <div className={styles.widgets}>
+      <WidgetLarge name="Calendar" icon={faCalendar} />
+      <WidgetSmall name="Notes" icon={faStickyNote} />
+    </div>
   );
 }
 
