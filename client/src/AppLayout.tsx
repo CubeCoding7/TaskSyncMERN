288fe3a9a9c899100809f6b7b461d5258e218f62
CubeCoding <sljohnston714@gmail.com>
2024-08-26 20:28:31 -0400

    redirected the user if logged in or not logged in

--- client/src/AppLayout.tsx
+++ client/src/AppLayout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "./components/navigation/SideBar";
 import AppHeader from "./components/navigation/AppHeader";
 import styles from "./AppLayout.module.css";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 const AppLayout: React.FC = () => {
+  const { user } = useAuthContext();
+
   return (
     <div className={styles.appContainer}>
+      {!user ? <Navigate to="/" replace={true} /> : null}
       <Sidebar />
       <AppHeader />
       <div className={styles.mainContent}>
