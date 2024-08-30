a5b87b46cf680bb9b85641ea3935afd72edf1ea0
CubeCoding <sljohnston714@gmail.com>
2024-08-30 11:32:54 -0400

    added the ability to logout

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,8 +1,10 @@
 import catchErrors from "../utils/catchErrors";
 import { createAccount, loginUser } from "../services/auth.service";
 import { CREATED, OK, UNAUTHORIZED } from "../constants/http";
-import { setAuthCookies } from "../utils/cookies";
+import { clearAuthCookies, setAuthCookies } from "../utils/cookies";
 import { loginSchema, registerSchema } from "./auth.schemas";
+import { verifyToken } from "../utils/jwt";
+import SessionModel from "../models/session.model";
 
 export const registerHandler = catchErrors(async (req, res) => {
   const request = registerSchema.parse({
@@ -25,5 +27,18 @@ export const loginHandler = catchErrors(async (req, res) => {
 
   return setAuthCookies({ res, accessToken, refreshToken }).status(OK).json({
     message: "Login successful",
+  });
+});
+
+export const logoutHandler = catchErrors(async (req, res) => {
+  const accessToken = req.cookies.accessToken;
+  const { payload } = verifyToken(accessToken);
+
+  if (payload) {
+    await SessionModel.findByIdAndDelete(payload.sessionId);
+  }
+
+  return clearAuthCookies(res).status(OK).json({
+    message: "Logout successful",
   });
 });
