15507028fdf45d741a4896faa4c4791851f42e43
CubeCoding <sljohnston714@gmail.com>
2024-08-31 15:12:50 -0400

    fixed an error

--- server/src/middleware/authenticate.ts
+++ server/src/middleware/authenticate.ts
@@ -3,7 +3,9 @@ import appAssert from '../utils/appAssert';
 import AppErrorCode from '../constants/appErrorCode';
 import { UNAUTHORIZED } from '../constants/http';
 import { verifyToken } from '../utils/jwt';
+import mongoose from 'mongoose';
 
+// wrap with catchErrors() if you need this to be async
 const authenticate: RequestHandler = (req, res, next) => {
 	const accessToken = req.cookies.accessToken as string | undefined;
 	appAssert(
@@ -21,8 +23,8 @@ const authenticate: RequestHandler = (req, res, next) => {
 		AppErrorCode.InvalidAccessToken
 	);
 
-	req.userId = payload.userId;
-	req.sessionId = payload.sessionId;
+	req.userId = payload.userId as mongoose.Types.ObjectId;
+	req.sessionId = payload.sessionId as mongoose.Types.ObjectId;
 	next();
 };
 
