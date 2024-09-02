6f533f8b1b7f7354cc8dfd98b6fc6d4835367270
CubeCoding <sljohnston714@gmail.com>
2024-09-02 16:48:03 -0400

    reverted a change made

--- server/src/controllers/task.controller.ts
+++ server/src/controllers/task.controller.ts
@@ -1,12 +1,11 @@
-// src/controllers/task.controller.ts
 import { Request, Response } from 'express';
 import Task from '../models/task.model';
 import mongoose from 'mongoose';
 
 const getTasks = async (req: Request, res: Response) => {
-	const user_id = req.userId;
+	// const user_id = req.user._id;
 
-	const tasks = await Task.find({ user_id }).sort({ createdAt: -1 });
+	const tasks = await Task.find({}).sort({ createdAt: -1 });
 
 	res.status(200).json(tasks);
 };
@@ -18,7 +17,7 @@ const getTask = async (req: Request, res: Response) => {
 		return res.status(404).json({ error: 'No such task' });
 	}
 
-	const task = await Task.findOne({ _id: id, user_id: req.userId });
+	const task = await Task.findById(id);
 
 	if (!task) {
 		return res.status(404).json({ error: 'No such task' });
@@ -41,21 +40,60 @@ const createTask = async (req: Request, res: Response) => {
 	if (!dueDate) {
 		emptyFields.push('dueDate');
 	}
-	if (emptyFields.length > 0) {
+	if (emptyFields.length! > 3) {
 		return res
 			.status(400)
 			.json({ error: 'Please fill in all the fields', emptyFields });
 	}
 
+	// if (!req.user) {
+	//   return res.status(401).json({ error: "Unauthorized" });
+	// }
+
 	try {
-		const user_id = req.userId;
-		const task = await Task.create({ name, description, dueDate, user_id });
-		res.status(201).json(task);
+		// const user_id = req.user._id;
+		const task = await Task.create({ name, description, dueDate });
+		res.status(200).json(task);
 	} catch (err) {
 		res.status(400).json({ error: (err as Error).message });
 	}
 };
 
-// ... other controllers ...
+const deleteTask = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: 'No such task' });
+	}
+
+	const task = await Task.findOneAndDelete({ _id: id });
+
+	if (!task) {
+		return res.status(404).json({ error: 'No such task' });
+	}
+
+	res.status(200).json(task);
+};
+
+const updateTask = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: 'No such task' });
+	}
+
+	const task = await Task.findOneAndUpdate(
+		{ _id: id },
+		{
+			...req.body,
+		}
+	);
+
+	if (!task) {
+		return res.status(404).json({ error: 'No such task' });
+	}
+
+	res.status(200).json(task);
+};
 
 export { getTasks, getTask, createTask, deleteTask, updateTask };
