7efe0c93d4634bdecb8926d29222a6854fe779e2
CubeCoding <sljohnston714@gmail.com>
2024-09-04 18:17:49 -0400

    added a better system to set sidebar buttons to active

--- client/src/components/navigation/SideBar/SideBar.tsx
+++ client/src/components/navigation/SideBar/SideBar.tsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React from 'react';
 import {
-  faHome,
-  faTasks,
-  faCalendarDays,
-  faStickyNote,
-} from "@fortawesome/pro-solid-svg-icons";
-import styles from "./SideBar.module.css";
-import SideBarButton from "./SideBarButton";
+	faHome,
+	faTasks,
+	faCalendarDays,
+	faStickyNote,
+} from '@fortawesome/pro-solid-svg-icons';
+import styles from './SideBar.module.css';
+import SideBarButton from './SideBarButton';
 
 const Sidebar: React.FC = () => {
-  return (
-    <div className={styles.sidebar}>
-      <div className={styles.iconSection}>
-        <SideBarButton href="/app/home" icon={faHome} />
-        <SideBarButton href="/app/tasks" icon={faTasks} />
-        <SideBarButton href="/app/calendar" icon={faCalendarDays} />
-        <SideBarButton href="/app/notes" icon={faStickyNote} />
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.sidebar}>
+			<div className={styles.iconSection}>
+				<SideBarButton href="/app/home" icon={faHome} activeLink="/app/home" />
+				<SideBarButton
+					href="/app/tasks/inbox"
+					icon={faTasks}
+					activeLink="/app/tasks"
+				/>
+				<SideBarButton
+					href="/app/calendar"
+					icon={faCalendarDays}
+					activeLink="/app/calendar"
+				/>
+				<SideBarButton
+					href="/app/notes"
+					icon={faStickyNote}
+					activeLink="/app/notes"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Sidebar;
--- client/src/components/navigation/SideBar/SideBarButton.tsx
+++ client/src/components/navigation/SideBar/SideBarButton.tsx
@@ -1,23 +1,26 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { NavLink } from "react-router-dom";
-import styles from "./SideBar.module.css";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NavLink } from 'react-router-dom';
+import styles from './SideBar.module.css';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 interface Props {
-  href: string;
-  icon: IconProp;
+	href: string;
+	icon: IconProp;
+	activeLink: string;
 }
 
-export default function SideBarButton({ href, icon }: Props) {
-  return (
-    <NavLink
-      to={href}
-      className={({ isActive }) =>
-        isActive ? `${styles.iconLink} ${styles.active}` : styles.iconLink
-      }
-    >
-      <div className={styles.iconActiveLine}></div>
-      <FontAwesomeIcon icon={icon} />
-    </NavLink>
-  );
+export default function SideBarButton({ href, icon, activeLink }: Props) {
+	return (
+		<NavLink
+			to={href}
+			className={() =>
+				window.location.pathname.startsWith(activeLink)
+					? `${styles.iconLink} ${styles.active}`
+					: styles.iconLink
+			}
+		>
+			<div className={styles.iconActiveLine}></div>
+			<FontAwesomeIcon icon={icon} />
+		</NavLink>
+	);
 }
