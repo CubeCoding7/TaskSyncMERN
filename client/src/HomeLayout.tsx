734927c3f9bc7e24c4646321a3ed554abec10c17
Samuel Johnston <95382436+CubeCoding7@users.noreply.github.com>
2024-09-18 20:48:38 +0000

    Made it so it updates the colors on the homepage.

--- client/src/HomeLayout.tsx
+++ client/src/HomeLayout.tsx
@@ -1,7 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import HomeHeader from './components/navigation/HomeHeader';
+import { parseColor, rgbToRgb } from './lib/colorUtils';
 
 function HomeLayout() {
+
+                document.documentElement.style.setProperty(
+                        '--first-color',
+                        '0, 4, 255'
+                );
+                const colorStr = '0, 4, 255';
+                const opacity = 0.6;
+                const { r, g, b } = parseColor(colorStr);
+                document.documentElement.style.setProperty(
+                        '--header-color',
+                        rgbaToRgb(r, g, b, opacity)
+                );
+                document.documentElement.style.setProperty(
+                        '--second-color',
+                         '0, 199, 123'
+                );
+
 	return (
 		<div className="App">
 			<HomeHeader />
