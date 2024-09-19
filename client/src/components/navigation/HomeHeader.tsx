1d1a81739a72e65bc109fd222408b87e8b6d9903
CubeCoding <sljohnston714@gmail.com>
2024-09-19 15:10:08 -0400

    reformated the home header for better styles

--- client/src/components/navigation/HomeHeader.tsx
+++ client/src/components/navigation/HomeHeader.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import styles from './HomeHeader.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/pro-solid-svg-icons';
 
 function HomeHeader() {
 	return (
@@ -10,22 +12,25 @@ function HomeHeader() {
 			<div className={styles.title}>
 				<Link to="/">TaskSync</Link>
 			</div>
+			<button className={styles.hamburger} onClick={toggleMobileMenu}>
+				<FontAwesomeIcon icon={faBars} />
+			</button>
 			<nav>
+				<Link to="/features" className={styles.nav_button}>
+					Features
+				</Link>
+				<Link to="/download" className={styles.nav_button}>
+					Download
+				</Link>
+				<Link to="/help" className={styles.nav_button}>
+					Help
+				</Link>
+				<div className={styles.headerDivider}></div>
 				<Link to="/register" className={styles.sign_up_button}>
 					Sign up for free
 				</Link>
 				<Link to="/login" className={styles.nav_button}>
 					Log in
-				</Link>
-				<div className={styles.headerDivider}></div>
-				<Link to="/help" className={styles.nav_button}>
-					Help
-				</Link>
-				<Link to="/download" className={styles.nav_button}>
-					Download
-				</Link>
-				<Link to="/features" className={styles.nav_button}>
-					Features
 				</Link>
 			</nav>
 		</header>
